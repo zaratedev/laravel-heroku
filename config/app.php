7723a3d22b2165242aab43dfdb8a2c521f3fc807
zaratedev <zaratedev@gmail.com>
2017-11-01 09:00:24 -0600

    Updating config app.php

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:VV9kx7vm3bXZ1JEPehXNck1VkzBKKLzTFdjQuHC9Mao='),
 
     'cipher' => 'AES-256-CBC',
 
