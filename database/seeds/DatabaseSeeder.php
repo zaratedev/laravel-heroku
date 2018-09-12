bfb16926b1337df7c3960553e3975b54fa2a5ae2
Jonathan Zarate <jonathan.zarate@dinkbit.com>
2018-09-12 12:29:38 -0500

    seeders products

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	DB::table('users')->delete();
+
+    	$products = [
+            [
+                'title' => 'Curso Laravel desde cero',
+                'description' => 'Aprende lo mas actual del framework mas popular de PHP',
+                'price' => 100,
+            ],
+            [
+                'title' => 'Curso Vue',
+                'description' => 'Crea aplicaciones de tipo SPA, con el framework que esta cambiando la web.',
+                'price' => 100,
+            ],
+            [
+                'title' => 'Curso Angular JS',
+                'description' => 'Crea aplicaciones increibles con Angular Js framework de Google',
+                'price' => 100,
+            ],
+            [
+                'title' => 'Curso de Estructuras de Datos en Java',
+                'description' => 'Curso dedicado a estructuras complejas de datos como Arboles, grafos, etc. Solo en Java.',
+                'price' => 100,
+            ],
+            [
+                'title' => 'Curso Bootstrap 4',
+                'description' => 'Diseño responsivo con el framework css más popular.',
+                'price' => 100,
+            ],
+        ];
+
+        // Uncomment the below to run the seeder
+        DB::table('products')->insert($products);
     }
 }
